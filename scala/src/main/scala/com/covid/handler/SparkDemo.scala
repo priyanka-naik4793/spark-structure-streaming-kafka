0149418f1aabc687fd74d8ba28cc28da24a97cc5
priyanka.naik <priyanka.naik@corelight.com>
2022-09-05 15:16:49 +0530

    spark demo for rdd added

--- scala/src/main/scala/com/covid/handler/SparkDemo.scala
+++ scala/src/main/scala/com/covid/handler/SparkDemo.scala
@@ -9,18 +9,12 @@ object SparkDemo {
       .builder
       .appName("Spark RDD demo")
       .config("spark.cassandra.connection.host", "localhost")
-      //      .config("spark.default.parallelism",4)
-       .config("spark.sql.shuffle.partitions",4)
       .getOrCreate()
 
     val rdd = spark.sparkContext.textFile("/home/priyanka.naik/Quovantis_Work_Space/spark-structure-streaming-kafka/scala/src/resources/demoSpark.txt")
-    println("rdd  partitions: ", rdd.getNumPartitions)
-    rdd.foreachPartition(p => println("Items in Partitions " + p.foreach(i => println(i))))
 
 
     val rdd1 = spark.sparkContext.parallelize(Array("etc", "fonts"))
-    println("rdd 1 partitions: ", rdd1.getNumPartitions)
-    rdd1.foreachPartition(p => println("Items in Partitions " + p.foreach(i => println(i))))
 
     val rdd_in = rdd.flatMap(line => line.split("/")).map(word => (word, 1)).reduceByKey(_ + _)
     val rdd1_in = rdd1.map(word => (word, 1)).reduceByKey(_ + _)
